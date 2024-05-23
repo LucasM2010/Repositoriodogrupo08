f358f08900d0935372488c31951d4f56ee1ec7d6
matheusantonio156 <164435308+matheusantonio156@users.noreply.github.com>
2024-05-23 16:05:45 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom/client";
 import Home from './routes/home';
 import Login from './routes/login';
 import CadastrarEmpresa from './routes/cadastrar-empresa';
+import LinhaDoTempo from './routes/linha-do-tempo'; // Importe o novo componente
 import {
   createBrowserRouter,
   RouterProvider,
@@ -10,18 +11,21 @@ import {
 import "./pages.css";
  
 const router = createBrowserRouter([
- 
   {
     path: "/",
-    element: <Login/>,
+    element: <Login />,
   },
   {
     path: "/home",
-    element: <Home/>,
+    element: <Home />,
   },
   {
-    path: "/cadastrar-empresa", // Adicione a nova rota
+    path: "/cadastrar-empresa",
     element: <CadastrarEmpresa />,
+  },
+  {
+    path: "/linha-do-tempo",
+    element: <LinhaDoTempo />, // Adicione a nova rota
   }
 ]);
  

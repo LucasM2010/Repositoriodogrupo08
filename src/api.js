7b5f46f7ef5c5a956206116bfb10b26cdf62a850
LucasM2010 <lucasggl2010@gmail.com>
2024-05-29 13:57:42 -0300

    Conexão axios

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:54025/api', 
+    baseURL: 'http://localhost:5173/api', 
 });
 
 export const getItems = () => api.get('/items');
